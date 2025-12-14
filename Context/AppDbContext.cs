127a8cfcd5e3e2e01bd38136232372a912eefb9b
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-14 15:40:11 -0500

    feat: add AppDbContext

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace MiniMazErpBack;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace MiniMazErpBack;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    DbSet<Buy> Buys { get; set; }
+    DbSet<Inventory> Inventories { get; set; }
+    DbSet<Movement> Movements { get; set; }
+    DbSet<Product> Products { get; set; }
+    DbSet<Sell> Sells { get; set; }
+    DbSet<Warehouse> Warehouses { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
 }
--- Model/Buy.cs
+++ Model/Buy.cs
@@ -1,6 +1,12 @@
-﻿namespace MiniMazErpBack;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
 
+namespace MiniMazErpBack;
+
+[Table("Buy")]
 public class Buy
 {
-
+    [Key]
+    public int id;
 }
