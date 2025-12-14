0b7bf29a11707c15a5ccdc42ae9a2345d6fb4f43
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-14 11:01:06 -0500

    add WebAppBuilderConfig

---
+++ Config/WebApplicationBuilderConfig.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace MiniMazErpBack;
+
+public class WebAppBuilderConfig
+{
+
+    public static void ConfigureBuilder(WebApplicationBuilder builder, string connectionString)
+    {
+        // Add services to the container.
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddOpenApi("v1"); // Customize the document name
+        // builder.Services.AddDbContext<AppDbContext>(optionsAction: options => options.UseNpgsql(connectionString));
+        // builder.Services.AddScoped<UserService>();
+        // builder.Services.AddScoped<TokenService>();
+        // TODO: map and register Mapster DI
+        builder.Services.AddControllers();
+
+    }
+
+    public static void ConfigureCorsPolicy(WebApplicationBuilder builder)
+    {
+        // Configure CORS
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowSpecificOrigin", policy =>
+            {
+                policy.WithOrigins("http://localhost:4200")
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+            });
+        });
+    }
+}
