c7e48b963a7a1184e5dfe97938b7a3b8477a9466
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-14 15:06:55 -0500

    feat: add database squeleton to code

---
+++ Config/WebAppConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace MiniMazErpBack;
+
+public class WebAppConfig
+{
+    public static void UseDevAppConfigs(WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+    }
+
+    public static void UseGeneralAppConfigs(WebApplication app)
+    {
+        app.UseCors("AllowSpecificOrigin");
+        app.UseHttpsRedirection();
+        app.MapControllers();
+        app.Run();
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -7,17 +7,11 @@ Env.Load();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-WebAppBuilderConfig.ConfigureBuilder(builder, Env.GetString("CONECCTION_STRING_POSTGRESQL"));
+
+WebAppBuilderConfig.ConfigureBuilder(builder, Env.GetString("CONNECTION_STRING_POSTGRESQL"));
 WebAppBuilderConfig.ConfigureCorsPolicy(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-
-app.UseCors("AllowSpecificOrigin");
-app.UseHttpsRedirection();
+WebAppConfig.UseDevAppConfigs(app);
+WebAppConfig.UseGeneralAppConfigs(app);
