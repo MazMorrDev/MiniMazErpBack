6891ebc3ffdeab133e0a1c925e7154c60de800b6
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-18 10:18:22 -0500

    feat: full map the expense entity

--- Model/Expense.cs
+++ Model/Expense.cs
@@ -1,9 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MiniMazErpBack;
 
 public class Expense
 {
+    [Key, Column("movement_id")]
+    public int? MovementId { get; set; }
+
     [Column("expense_type")]
     public ExpenseType ExpenseType { get; set; }
+
+    [Column("total_price")]
+    public double TotalPrice { get; set; }
+
+    // Navigation Properties
+    [ForeignKey("MovementId"), Required]
+    public required virtual Movement Movement { get; set; } 
+
 }
