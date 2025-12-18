ed360e81a49b7bde5087281fd44e3180f9bbbb37
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-18 09:05:55 -0500

    feat: map Warehouse entity

--- Model/Warehouse.cs
+++ Model/Warehouse.cs
@@ -1,6 +1,18 @@
-﻿namespace MiniMazErpBack;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MiniMazErpBack;
+
+[Table("Warehouse")]
 public class Warehouse
 {
+    [Key, Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
 
+    [Required, Column("name"), MaxLength(30)]
+    public string Name { get; set; } = string.Empty;
+
+    [Column("description"), MaxLength(255)]
+    public string? Description { get; set; }
 }
