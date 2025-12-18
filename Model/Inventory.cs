429dc6db5818bb69e044bfd8e3ca053c1269041e
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-18 08:14:00 -0500

    feat: map inventory entity

--- Model/Inventory.cs
+++ Model/Inventory.cs
@@ -1,6 +1,41 @@
-﻿namespace MiniMazErpBack;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MiniMazErpBack;
+
+[Table("Inventory")]
 public class Inventory
 {
+    [Key, Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
 
+    [Required, Column("warehouse_id")]
+    public int WarehouseId { get; set; }
+
+    [Required, Column("product_id")]
+    public int ProductId { get; set; }
+
+    [Required, Column("stock")]
+    public int Stock { get; set; }
+
+    // // Lógica para alertas
+    // public bool IsBelowAlertStock => AlertStock.HasValue && Stock < AlertStock.Value;
+
+    // public bool IsBelowWarningStock => WarningStock.HasValue && Stock < WarningStock.Value;
+
+    [Column("alert_stock")]
+    public int? AlertStock { get; set; }
+
+    [Column("warning_stock")]
+    public int? WarningStock { get; set; }
+
+    // Navigation Properties
+    [ForeignKey("WarehouseId")]
+    public virtual Warehouse Warehouse { get; set; } = null!;
+
+    [ForeignKey("ProductId")]
+    public virtual Product Product { get; set; } = null!;
+
+    public virtual ICollection<Movement> Movements { get; set; } = new HashSet<Movement>();
 }
