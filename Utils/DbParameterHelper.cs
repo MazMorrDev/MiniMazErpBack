4a3cf67acefb03d72731db706aa874b1e8893182
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-25 09:17:48 -0500

    feat: add nullable to object parameter in CreateParameter()

--- Utils/DbParameterHelper.cs
+++ Utils/DbParameterHelper.cs
@@ -5,7 +5,7 @@ namespace MiniMazErpBack;
 public class DbParameterHelper
 {
     // Método helper para crear parámetros de forma segura
-    public static DbParameter CreateParameter(DbCommand command, string name, object value)
+    public static DbParameter CreateParameter(DbCommand command, string name, object? value)
     {
         var parameter = command.CreateParameter();
         parameter.ParameterName = name;
