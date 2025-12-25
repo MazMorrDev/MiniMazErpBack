c77a6db3e48ded9640924506a7aa55278ee06ea6
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-25 11:44:14 -0500

    feat: add IInventoryService functions

--- Services/Interfaces/IInventoryService.cs
+++ Services/Interfaces/IInventoryService.cs
@@ -2,5 +2,9 @@
 
 public interface IInventoryService
 {
-
+    Task<Inventory> CreateInventoryAsync(Inventory inventory);
+    Task<bool> DeleteInventoryAsync(int id);
+    Task<IEnumerable<Inventory>> GetAllInventoriesAsync();
+    Task<Inventory?> GetInventoryByIdAsync(int id);
+    Task<bool> UpdateInventoryAsync(Inventory inventory);
 }
--- Services/InventoryService.cs
+++ Services/InventoryService.cs
@@ -2,5 +2,28 @@
 
 public class InventoryService : IInventoryService
 {
+    public Task<Inventory> CreateInventoryAsync(Inventory inventory)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<bool> DeleteInventoryAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Inventory>> GetAllInventoriesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Inventory?> GetInventoryByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateInventoryAsync(Inventory inventory)
+    {
+        throw new NotImplementedException();
+    }
 }
