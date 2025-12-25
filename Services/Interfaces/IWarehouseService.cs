709d93fb074e8be291b6aa35255bc894ad0cc087
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-25 11:53:43 -0500

    feat: add IService functions

--- Services/Interfaces/IWarehouseService.cs
+++ Services/Interfaces/IWarehouseService.cs
@@ -2,5 +2,9 @@
 
 public interface IWarehouseService
 {
-
+    Task<Warehouse> CreateWarehouseAsync(Warehouse warehouse);
+    Task<bool> DeleteWarehouseAsync(int id);
+    Task<IEnumerable<Warehouse>> GetAllWarehousesAsync();
+    Task<Warehouse?> GetWarehouseByIdAsync(int id);
+    Task<bool> UpdateWarehouseAsync(Warehouse warehouse);
 }
--- Services/WarehouseService.cs
+++ Services/WarehouseService.cs
@@ -2,5 +2,28 @@
 
 public class WarehouseService : IWarehouseService
 {
+    public Task<Warehouse> CreateWarehouseAsync(Warehouse warehouse)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<bool> DeleteWarehouseAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Warehouse>> GetAllWarehousesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Warehouse?> GetWarehouseByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateWarehouseAsync(Warehouse warehouse)
+    {
+        throw new NotImplementedException();
+    }
 }
