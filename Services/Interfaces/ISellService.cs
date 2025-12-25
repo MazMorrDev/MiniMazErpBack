dcd77fc950afc9ed29fdf1d4e86b04afd16b5016
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-25 11:48:52 -0500

    feat: add ISellService functions

--- Services/Interfaces/ISellService.cs
+++ Services/Interfaces/ISellService.cs
@@ -2,5 +2,9 @@
 
 public interface ISellService
 {
-
+    Task<Sell> CreateSellAsync(Sell sell);
+    Task<bool> DeleteSellAsync(int id);
+    Task<IEnumerable<Sell>> GetAllSellsAsync();
+    Task<Sell?> GetSellByIdAsync(int id);
+    Task<bool> UpdateSellAsync(Sell sell);
 }
--- Services/SellService.cs
+++ Services/SellService.cs
@@ -2,5 +2,28 @@
 
 public class SellService : ISellService
 {
+    public Task<Sell> CreateSellAsync(Sell sell)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<bool> DeleteSellAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Sell>> GetAllSellsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Sell?> GetSellByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateSellAsync(Sell sell)
+    {
+        throw new NotImplementedException();
+    }
 }
