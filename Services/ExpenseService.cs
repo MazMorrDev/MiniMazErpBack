582ba5f66cf5baf597f44a22bfc65377f710bccb
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-25 11:38:29 -0500

    feat: add IExpenseService functions

--- Services/ExpenseService.cs
+++ Services/ExpenseService.cs
@@ -2,5 +2,28 @@
 
 public class ExpenseService : IExpenseService
 {
+    public Task<Expense> CreateExpenseAsync(Expense expense)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<bool> DeleteExpenseAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Expense>> GetAllExpenseAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Expense?> GetExpenseByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateExpenseAsync(Expense expense)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Services/Interfaces/IExpenseService.cs
+++ Services/Interfaces/IExpenseService.cs
@@ -2,5 +2,9 @@
 
 public interface IExpenseService
 {
-
+    Task<Expense> CreateExpenseAsync(Expense expense);
+    Task<bool> DeleteExpenseAsync(int id);
+    Task<IEnumerable<Expense>> GetAllExpenseAsync();
+    Task<Expense?> GetExpenseByIdAsync(int id);
+    Task<bool> UpdateExpenseAsync(Expense expense);
 }
