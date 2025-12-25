bd1a9725ceea1ffc062b68e4e5855201d27f62f1
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-25 09:16:22 -0500

    feat: add ProductRepository logic

--- Repository/ProductRepository.cs
+++ Repository/ProductRepository.cs
@@ -1,29 +1,121 @@
-﻿namespace MiniMazErpBack;
+﻿﻿using System.Data.Common;
 
-public class ProductRepository : IProductRepository
+namespace MiniMazErpBack;
+
+public class ProductRepository(Func<DbConnection> factory) : IProductRepository
 {
-    public Task<int> CreateAsync(Product product)
+    private readonly Func<DbConnection> _connectionFactory = factory;
+
+    public async Task<int> CreateAsync(Product product)
     {
-        throw new NotImplementedException();
+        using var connection = _connectionFactory();
+        await connection.OpenAsync();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = """
+            INSERT INTO "Product" (name, sell_price)
+            VALUES (@name, @sell_price)
+            RETURNING id;
+            """;
+        
+        command.Parameters.Add(DbParameterHelper.CreateParameter(command, "@name", product.Name));
+        command.Parameters.Add(DbParameterHelper.CreateParameter(command, "@sell_price", product.SellPrice));
+
+        return Convert.ToInt32(await command.ExecuteScalarAsync());
     }
 
-    public Task<bool> DeleteAsync(int id)
+    public async Task<bool> DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        using var connection = _connectionFactory();
+        await connection.OpenAsync();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = """
+            DELETE FROM "Product" 
+            WHERE id = @id;
+            """;
+        
+        command.Parameters.Add(DbParameterHelper.CreateParameter(command, "@id", id));
+
+        return await command.ExecuteNonQueryAsync() > 0;
     }
 
-    public Task<IEnumerable<Product>> GetAllAsync()
+    public async Task<IEnumerable<Product>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        using var connection = _connectionFactory();
+        await connection.OpenAsync();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = """
+            SELECT id, name, sell_price
+            FROM "Product"
+            ORDER BY name;
+            """;
+
+        using var reader = await command.ExecuteReaderAsync();
+        var result = new List<Product>();
+
+        while (await reader.ReadAsync())
+        {
+            result.Add(MapToProduct(reader));
+        }
+
+        return result;
     }
 
-    public Task<Product?> GetByIdAsync(int id)
+    public async Task<Product?> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        using var connection = _connectionFactory();
+        await connection.OpenAsync();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = """
+            SELECT id, name, sell_price
+            FROM "Product"
+            WHERE id = @id;
+            """;
+        
+        command.Parameters.Add(DbParameterHelper.CreateParameter(command, "@id", id));
+
+        using var reader = await command.ExecuteReaderAsync();
+        
+        if (await reader.ReadAsync())
+        {
+            return MapToProduct(reader);
+        }
+
+        return null;
     }
 
-    public Task<bool> UpdateAsync(Product product)
+    public async Task<bool> UpdateAsync(Product product)
     {
-        throw new NotImplementedException();
+        using var connection = _connectionFactory();
+        await connection.OpenAsync();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = """
+            UPDATE "Product" 
+            SET name = @name,
+                sell_price = @sell_price
+            WHERE id = @id;
+            """;
+        
+        command.Parameters.Add(DbParameterHelper.CreateParameter(command, "@name", product.Name));
+        command.Parameters.Add(DbParameterHelper.CreateParameter(command, "@sell_price", product.SellPrice));
+        command.Parameters.Add(DbParameterHelper.CreateParameter(command, "@id", product.Id));
+
+        return await command.ExecuteNonQueryAsync() > 0;
+    }
+
+    private static Product MapToProduct(DbDataReader reader)
+    {
+        return new Product
+        {
+            Id = reader.GetInt32(reader.GetOrdinal("id")),
+            Name = reader.GetString(reader.GetOrdinal("name")),
+            SellPrice = reader.IsDBNull(reader.GetOrdinal("sell_price")) 
+                ? null 
+                : reader.GetDecimal(reader.GetOrdinal("sell_price"))
+        };
     }
 }
