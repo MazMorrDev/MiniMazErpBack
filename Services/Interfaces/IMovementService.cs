625a4fc0e91cc9b83af9b3f60c449f00cc464c76
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-25 11:47:06 -0500

    feat: add IMovementService functions

--- Services/Interfaces/IMovementService.cs
+++ Services/Interfaces/IMovementService.cs
@@ -2,5 +2,9 @@
 
 public interface IMovementService
 {
-
+    Task<Movement> CreateMovementAsync(Movement movement);
+    Task<bool> DeleteMovementAsync(int id);
+    Task<IEnumerable<Movement>> GetAllMovementsAsync();
+    Task<Movement?> GetMovementByIdAsync(int id);
+    Task<bool> UpdateMovementAsync(Movement movement);
 }
--- Services/MovementService.cs
+++ Services/MovementService.cs
@@ -2,5 +2,28 @@
 
 public class MovementService : IMovementService
 {
+    public Task<Movement> CreateMovementAsync(Movement movement)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<bool> DeleteMovementAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Movement>> GetAllMovementsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Movement?> GetMovementByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateMovementAsync(Movement movement)
+    {
+        throw new NotImplementedException();
+    }
 }
