2e5f30c1fd6aafd64c6a6837f55ea6642cde965c
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-25 11:35:41 -0500

    feat: add IBuyService functions

--- Services/BuyService.cs
+++ Services/BuyService.cs
@@ -2,5 +2,28 @@
 
 public class BuyService : IBuyService
 {
+    public Task<Buy> CreateBuyAsync(Buy buy)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<bool> DeleteBuyAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Buy>> GetAllBuysAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Buy?> GetBuyByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateBuyAsync(Buy buy)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Services/Interfaces/IBuyService.cs
+++ Services/Interfaces/IBuyService.cs
@@ -2,5 +2,9 @@
 
 public interface IBuyService
 {
-
+    Task<Buy> CreateBuyAsync(Buy buy);
+    Task<bool> DeleteBuyAsync(int id);
+    Task<IEnumerable<Buy>> GetAllBuysAsync();
+    Task<Buy?> GetBuyByIdAsync(int id);
+    Task<bool> UpdateBuyAsync(Buy buy);
 }
