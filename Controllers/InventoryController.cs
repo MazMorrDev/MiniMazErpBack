0c940145e963b9e3823648f3fa1c28756916c957
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2025-12-30 20:31:24 -0500

    feat: add Inventory and Products controller methods

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -2,7 +2,74 @@
 
 namespace MiniMazErpBack;
 
-public class InventoryController(InventoryService service) : ControllerBase
+[ApiController]
+[Route("api/[controller]")]
+public class InventoryController(IInventoryService service) : ControllerBase
 {
     private readonly IInventoryService _service = service;
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateInventoryDto inventoryDto)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+        }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, UpdateInventoryDto inventoryDto)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor {ex.Message}");
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor {ex.Message}");
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor {ex.Message}");
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor {ex.Message}");
+        }
+    }
 }
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,7 +2,72 @@
 
 namespace MiniMazErpBack;
 
-public class ProductController(ProductService service): ControllerBase
+public class ProductController(ProductService service) : ControllerBase
 {
     private readonly IProductService _service = service;
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateProductDto productDto)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+        }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(int id, UpdateProductDto productDto)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor {ex.Message}");
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor {ex.Message}");
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor {ex.Message}");
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error interno del servidor {ex.Message}");
+        }
+    }
 }
